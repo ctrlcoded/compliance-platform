5d33316f27944858139ee1d356d15b876bf757cd
Aryan <22bcs029@nith.ac.in>
2026-03-04 00:56:17 +0530

    chore: completely remove backend node_modules and logs

--- backend/dist/core/application/dto/ComplianceDTO.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.computeCbQuerySchema = void 0;
-const zod_1 = require("zod");
-exports.computeCbQuerySchema = zod_1.z.object({
-    shipId: zod_1.z.string().min(1, 'shipId is required'),
-    year: zod_1.z.coerce.number().int().min(2025, 'Year must be >= 2025'),
-});
